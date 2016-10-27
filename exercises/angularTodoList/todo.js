a8d5966016c223c10d7064e6a4b863059302aee1
Meheret Egzerab <meheretgirma22@gmail.com>
2016-10-27 16:53:45 -0400

    fixed the edit button on the todolist

--- exercises/angularTodoList/todo.js
+++ exercises/angularTodoList/todo.js
@@ -1,40 +1,41 @@
-var app = angular.module("myApp",[]);
-app.controller("myCtrl",["$scope","$http" ,function($scope,$http){
-   
-    
-  $http.get("http://api.vschool.io/meheret/todo")
-            .then(function(response){
-           $scope.todolist = response.data;
-       })
-                         
-//$scope.sendData = function(){
-//    var data = {
-//        title: $scope.title,
-//        lName:
-//    }
-//}
- 
-$scope.createtodo = function(){
-    
-    $http.post("http://api.vschool.io/meheret/todo", $scope.todo ).then(function(response){
-     $scope.todolist.push(response.data);
-    })
-}
+var app = angular.module("myApp", []);
+app.controller("myCtrl", ["$scope", "$http", function ($scope, $http) {
+    $scope.editMode = false;
 
-$scope.deletetodo = function(item, index){
-   // item_to_delete = $scope.todolist[idx];
-       // console.log(item_to_delete);
-    $http.delete("http://api.vschool.io/meheret/todo/" + item._id)
-        .then(function(response){
-           $scope.todolist.splice(index,1);
-    })
-}
+    $http.get("http://api.vschool.io/meheret/todo")
+        .then(function (response) {
+            $scope.todolist = response.data;
+        })
 
-//$scope.edittodo = function()
-//    $http.put("http://api.vschool.io/meheret/todo" +  $scope.item.id)
-//        .then(function(response){
-//            console.log(response)
-//})
+    $scope.createtodo = function () {
 
-                         
+        $http.post("http://api.vschool.io/meheret/todo", $scope.todo).then(function (response) {
+            $scope.todolist.push(response.data);
+        })
+    }
+
+    $scope.deletetodo = function (item, index) {
+        // item_to_delete = $scope.todolist[idx];
+        // console.log(item_to_delete);
+        $http.delete("http://api.vschool.io/meheret/todo/" + item._id)
+            .then(function (response) {
+                $scope.todolist.splice(index, 1);
+            })
+    }
+
+    $scope.updatetodo = function (item) {
+        $http.put("http://api.vschool.io/meheret/todo/" + item._id, item)
+            .then(function (response) {
+                console.log(response);
+                item.editMode = false;
+            });
+    }
+
+    //$scope.edittodo = function()
+    //    $http.put("http://api.vschool.io/meheret/todo" +  $scope.item.id)
+    //        .then(function(response){
+    //            console.log(response)
+    //})
+
+
 }])
