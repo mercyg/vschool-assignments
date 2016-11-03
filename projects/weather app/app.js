991440e5be1cff27d3b3997f0981ef06a05fe2f0
Meheret Egzerab <meheretgirma22@gmail.com>
2016-11-03 14:42:50 -0400

    fixed ng-route

--- projects/weather app/app.js
+++ projects/weather app/app.js
@@ -1,4 +1,15 @@
-var app = angular.module("myApp", ['ngGeolocation'])
+var app = angular.module("myApp", ['ngGeolocation', 'ngRoute'])
+
+.config(function($routeProvider){
+    $routeProvider.when('/',{
+        controller: 'searchController',
+        templateUrl: 'template/search.html'
+    })
+        .when('/city',{
+        controller: 'cityController',
+        templateUrl: 'template/city.html'
+    })
+})
 
 app.service("WeatherService", ["$geolocation", "$http", function ($geolocation, $http) {
     var self = this;
@@ -12,17 +23,21 @@ app.service("WeatherService", ["$geolocation", "$http", function ($geolocation, 
             })
     }
     this.weather = function (long, lat) {
-        var baseUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=18a926c00a15bf7072316e88506a8add&units=metric"
+//     var baseUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=18a926c00a15bf7072316e88506a8add&units=metric"
+        var baseUrl = "https://api.darksky.net/forecast/37b36f0ccffe23b0d9ec4169e1fa9afc/" + lat + "," + long;
         return $http.get(baseUrl);
     }
 }])
 
+app.controller('cityController',['$scope', function($scope){
+    
+}])
 
 app.controller("myCtrl", ["$scope", "WeatherService", function ($scope, WeatherService) {
 
     WeatherService.getLocation().then(function (response) {
-        $scope.temp = response.data.main.temp;
-       $scope.describe = response.data.weather[0].description;
-        
+        $scope.temp = response.data.currently.temperature;
+       $scope.describe = response.data.currently.icon;
+
     });
 }]);
